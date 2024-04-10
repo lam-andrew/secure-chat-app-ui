aef3a811d71e9a392e111c4441f0831b8c010776
lam-andrew <andrewlam2193@gmail.com>
2024-04-09 22:34:41 -0700

    app allows for google oauth sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google';
 import { SidebarProvider } from './components/SidebarContext';
 import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
 
 const App: React.FC = () => {
   const [isSidebarOpen] = useState(true);
+  const responseMessage = (response: any) => {
+    console.log(response);
+  };
+  const errorMessage = (error: any) => {
+    console.log(error);
+  };
   return (
-    <SidebarProvider>
-      <div className="flex h-screen p-4 bg-slate-900">
-        <Sidebar />
-        <Chat className={`${isSidebarOpen ? 'md:w-3/4 lg:w-3/4' : 'w-full'}`} />
-      </div>
-    </SidebarProvider>
+    <>
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage as any} />
+      <SidebarProvider>
+        <div className="flex h-screen p-4 bg-slate-900">
+          <Sidebar />
+          <Chat className={`${isSidebarOpen ? 'md:w-3/4 lg:w-3/4' : 'w-full'}`} />
+        </div>
+      </SidebarProvider>
+    </>
   );
 }
 
