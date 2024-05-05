b0ea45e1a4098e825c39362753971934e6689944
lam-andrew <andrewlam2193@gmail.com>
2024-05-05 16:51:05 -0700

    online users count

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -105,11 +105,7 @@ const Sidebar: React.FC<SidebarProps> = ({ socket }) => {
       {!isSidebarOpen && <OpenSidebarButton />}
       <div className={`mt-16 rounded transition-all duration-300 ease-in-out ${isSidebarOpen ? "w-full md:w-1/2 lg:w-1/6" : "w-0"} mr-3 bg-slate-800 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-slate-800`}>
         <div className="flex items-center justify-between ml-1 mt-4">
-          <input
-            className='flex items-center p-2 mx-1 text-white bg-slate-900 shadow rounded-lg w-11/12 outline-none'
-            type="search"
-            placeholder="Search"
-          />
+          <div className='flex items-center p-2 mx-1 text-white rounded-lg w-11/12 outline-none'>ONLINE - {users.length}</div>
           <button onClick={() => setIsSidebarOpen(false)} className="mx-4 text-white">
             <img src={backArrow} className='text-white' alt="Close" />
           </button>
